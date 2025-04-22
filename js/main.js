e86a3e706bddc333274523ef282958754742093a
Kevin <kjm520@gmail.com>
2025-04-21 21:24:53 -0500

    module test

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { fetchLoadCount } from "./fetchLoadCount.js";
+
+
+const el = document.getElementById("sheetNumber");
+el.innerText = "Loading…";
+
+fetchLoadCount()
+  .then(value => {
+    el.innerText = value;
+  })
+  .catch(err => {
+    console.error("Fetch failed", err);
+    el.innerText = "Unable to load data.";
+  });
+
